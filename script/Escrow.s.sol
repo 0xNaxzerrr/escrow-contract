79bf761a702209128ceee9e9b2c094e1a240822a
0xNaxzerrr <naxdev75@gmail.com>
2024-11-02 07:02:30 +0100

    :sparkles: Address of buyer & seller & arbiter added

--- script/Escrow.s.sol
+++ script/Escrow.s.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// script/Escrow.s.sol
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
@@ -7,12 +7,14 @@ import {Escrow} from "../src/Escrow.sol";
 contract EscrowScript is Script {
     Escrow public escrow;
 
-    function setUp() public {}
+    address _buyer = address(0x1);
+    address _seller = address(0x2);
+    address _arbiter = address(0x3);
 
     function run() public {
         vm.startBroadcast();
 
-        escrow = new Escrow();
+        escrow = new Escrow(_buyer, _seller, _arbiter);
 
         vm.stopBroadcast();
     }
